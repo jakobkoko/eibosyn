7e8507914eb2046725014a0edca20a55da64fba3
hortus0 <paulschade98@googlemail.com>
2018-01-23 11:24:32 +0100

    Added TopContainer to view

--- src/view/TopContainer.java
+++ src/view/TopContainer.java
@@ -5,8 +5,10 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
-public class TopContainer {
+public class TopContainer extends HBox {
 
 	private MenuBar menuBar;
 	private Menu fileMenu;
@@ -14,15 +16,19 @@ public class TopContainer {
 	private MenuItem saveMenuItem;
 	
 	public TopContainer() {
+		this.setWidth(Double.MAX_VALUE);
 		
 		menuBar = new MenuBar();
-		fileMenu = new Menu();
+		HBox.setHgrow(menuBar, Priority.ALWAYS);
+		
+		fileMenu = new Menu("File");
 		openMenuItem = new MenuItem("Open Preset..", new ImageView(new Image("resources/preset.png")));
-		saveMenuItem = new MenuItem("Save Preset..");	
+		saveMenuItem = new MenuItem("Save Preset..", new ImageView(new Image("resources/save.png")));	
 		
 		fileMenu.getItems().addAll(openMenuItem, saveMenuItem);
 		menuBar.getMenus().add(fileMenu);
-		int x;
+		
+		this.getChildren().add(menuBar);
 	}
 	
 	

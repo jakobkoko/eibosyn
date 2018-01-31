3a954444b16f9df397a72dd7cf20cd88b5aade0c
np1e <nick.hafkemeyer@gmail.com>
2018-01-31 16:20:46 +0100

    added comments for Recorder

--- src/model/Recorder.java
+++ src/model/Recorder.java
@@ -2,40 +2,51 @@ package model;
 
 import ddf.minim.AudioOutput;
 import ddf.minim.AudioRecorder;
+import ddf.minim.Recordable;
 import de.hsrm.mi.eibo.simpleplayer.SimpleMinim;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 
 import java.io.File;
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
 public class Recorder {
 
     private AudioRecorder recorder;
-    FileChooser chooser;
-    SimpleMinim minim;
-    AudioOutput output;
+    private FileChooser chooser;
+    private SimpleMinim minim;
+    private Recordable output;
     private static final SimpleDateFormat sdf = new SimpleDateFormat("HH-mm-ss_yyy-MM-dd");
+    private boolean recording;
 
-    public Recorder(AudioOutput o, SimpleMinim m) {
+    /**
+     * constructs new Recorder
+     * @param o Recordable, which serves as source for the Recorder
+     * @param m SimpleMinim
+     */
+    public Recorder(Recordable o, SimpleMinim m) {
         minim = m;
         output = o;
         recorder = m.createRecorder(o, "default.wav");
         chooser = new FileChooser();
+        recording = false;
     }
 
+    /**
+     * creates new AudioRecorder and begins recording, when currently not recording.
+     * Ends recording and saves the audio ele
+     */
     public void toggleRecord() {
         Timestamp t = new Timestamp(System.currentTimeMillis());
         String timestamp = sdf.format(t);
-        if (!recorder.isRecording()) {
+        if (!recording) {
             recorder = minim.createRecorder(output, "record_" + timestamp + ".wav");
             recorder.beginRecord();
-        } else if (recorder.isRecording()) {
+            recording = true;
+        } else if (recording) {
             endRecord();
             recorder.save();
+            recording = false;
         }
     }
 
